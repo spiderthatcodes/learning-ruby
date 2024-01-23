33fb17dfdc5165471b799b4d9daadc2de3960c43
spiderthatcodes <jessicaldickerson@outlook.com>
2024-01-23 06:36:10 -0700

    just starting

---
+++ intro.rb
@@ -0,0 +1,12 @@
+# comment
+greeting = "Another hello"
+
+puts 'Hello, World!'
+print 'Hello again'
+print greeting
+
+def say_hello(whatever)
+    puts whatever
+end
+
+say_hello("blah")
---
+++ strings.rb
@@ -0,0 +1,3 @@
+# concat
+
+# interpolation
